913dd7ca1e55d2738667c20b86aca9a72def6578
Timothée Boiton <timothee.boiton@gmail.com>
2024-10-17 16:17:54 +0200

    Initial commit first part

---
+++ Assets/Scripts/Character/AttackScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+  
+    public void LightAttack(Animator animator)
+    {
+
+
+    }
+
+    public void HeavyAttack(Animator animator)
+    {
+
+
+    }
+
+
+
+}
